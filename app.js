eda24cc8584eb646d4b4bcf7c451ed84e16a409b
kostya-yaremenko <kostul2008@gmail.com>
2016-11-10 01:42:54 +0200

    d

--- app.js
+++ app.js
@@ -40,10 +40,11 @@ app.set('view engine', 'jade');
 ///////////////////////////////////////////////////////////
 
 
-var db = 'mongodb://localhost/db';
+// var db = 'mongodb://localhost/db';
+var db = 'mongodb://77.47.197.19:27017/db';
 // var db = 'mongodb://192.168.1.101:27017/db';
 mongoose.Promise = global.Promise;
-// mongoose.connect(db);
+mongoose.connect(db);
 
 app.get('/', function(req, res){
     res.send('happy to be here');
